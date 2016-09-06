9ab8cb777032006eeeb1f6ab18dd47f1d579ce01
Sara Grandfield <sara.grandfield@gmail.com>
2016-09-06 18:17:18 -0400

    Initial Commit

--- INFO450Multiply/INFO450Multiply.cpp
+++ INFO450Multiply/INFO450Multiply.cpp
@@ -1,11 +1,37 @@
-// INFO450Multiply.cpp : Defines the entry point for the console application.
-//
+// INFO450Multiply.cpp : Creates a Multiplication Table on the console using c++, for VCU INFO 450 
+// Sara Anne Grandfield 9/6/2016
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;
 
 
 int main()
 {
-    return 0;
+	
+	cout << "Multiplication Table:" << endl;
+	for (int x = 1; x < 10; x++)
+	{
+		if (x==1)
+		{
+			cout << "   ";
+			for (int f = 1; f < 10; f++)
+			{
+				cout << f << '\t';
+			}
+		}
+		else
+		{
+			cout << x << "| ";
+
+			for (int y = 1; y < 10; y++)
+			{
+				cout << y*x << '\t';
+			}
+		}
+		cout << endl;
+	}
+	return 0;
 }
 
